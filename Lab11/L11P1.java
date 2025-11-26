4bf884395e78f2c2c6b41ec7329ca597ecd018df
Sean Walborn <167147950+SeanWalborn@users.noreply.github.com>
2025-11-26 10:37:57 -0800

    Add files via upload

---
+++ Lab11/L11P1.java
@@ -0,0 +1,67 @@
+// Name: Sean Walborn
+// Lab 11, Problem 1
+// Submission date: 11/23/25
+
+/*
+ *  - demonstrates knowledge and implementation  of priority queue
+ *  - takes a file with a list of integers representing priorities
+ *  - 0 is a pop operation
+ *  - the higher the number the higher the priority
+ *  - the numbers are read one by one, if its higher than 0 its added to the queue
+ *  - if the number is zero the highest priority number is removed from the queue
+ *  - the queue and the popped number are displayed to the user every time a new number is popped
+ */
+
+package Lab11;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class L11P1 {
+	public static void main(String[] args) {
+		// setting up variables
+		String fileName = "";	
+		PriorityQueue<Integer> priQueue = new PriorityQueue<>(Collections.reverseOrder());
+		
+		// setting up scanner
+		Scanner scan = new Scanner(System.in);
+		
+		// collecting file name
+		System.out.print("Enter file name: ");
+		fileName = scan.next();
+		
+		// opening file
+		try {
+			// setting up to open file and scan
+			File priorityFile = new File(fileName);
+			Scanner scan2 = new Scanner(priorityFile);
+			
+			// building priority Queue
+			while(scan2.hasNextLine()) {
+				int currentNum = scan2.nextInt();
+				
+				// determine what to do with current number
+				if(currentNum == 0) {
+					// popping highest priority number
+					currentNum = priQueue.poll();
+					// printing the priority queue and popped number
+					System.out.println(currentNum + " popped");
+					System.out.println("Priority Queue: " + priQueue);	
+				}else {
+					// adding number to the queue
+					priQueue.add(currentNum);
+					
+				}
+			}
+			scan2.close();
+		}
+		catch(FileNotFoundException e) {
+			System.out.println("File Not Found");
+			e.printStackTrace();
+		}
+		scan.close();
+	}
+}
---
+++ Lab11/L11P2.java
@@ -0,0 +1,55 @@
+// Name: Sean Walborn
+// Lab 11, Problem 2
+// Submission date: 11/23/25
+
+/*
+ *  - demonstrates knowledge and implementation of priority queue
+ *  - collects the number of numbers, and the kth largest, and then each number all from user input
+ *  - finds the kth number and displays the number for the user
+ *  - all numbers are unique
+ *  - time complexity is linear and uses priority queue
+ *  - no adjustment made for output formating 1st, 2nd, 3rd etc
+ */
+
+package Lab11;
+
+import java.util.Scanner;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class L11P2 {
+	public static void main(String[] args) {
+		// setting up variables
+		int numData = 0;
+		int k = 0;
+		int kthNum = 0;
+		PriorityQueue<Integer> priQueue = new PriorityQueue<>(Collections.reverseOrder());
+		
+		
+		// setting up scanner
+		Scanner scan = new Scanner(System.in);
+		
+		// collecting data num and k
+		System.out.print("Enter number of data: ");
+		numData = scan.nextInt();
+		System.out.print("\nEnter k: ");
+		k = scan.nextInt();
+		System.out.println();
+		
+		// collecting data
+		for (int i = 0; i < numData; i++) {
+			priQueue.add(scan.nextInt());
+		}
+		
+		// moving to Kth largest number
+		for (int i = 1; i < k; i++) {
+			priQueue.poll();
+		}
+		
+		// removing and displaying kth largest number
+		kthNum = priQueue.poll();
+		System.out.print("The " + k + "th largest number is: " + kthNum);
+			
+		scan.close();		
+	}
+}
